0bd48c0b729c34ad3f1e3c9ac97a79f4a1da216f
Jônatas Livramento <jonatas.livramento@gmail.com>
2022-06-25 17:56:36 -0300

    Implementado pagina de vídeo e também rotas

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,14 +2,59 @@ import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-reac
 import { DefaultUi, Player, Youtube } from "@vime/react";
 
 import "@vime/core/themes/default.css";
+import { gql, useQuery } from "@apollo/client";
 
-export function Video() {
+const GET_LESSON_BY_SLUG = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+}
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    }
+  }
+}
+
+type VideoProps = {
+  lessonSlug: string;
+}
+
+export function Video({ lessonSlug }: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG, {
+    variables: {
+      slug: lessonSlug
+    }
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1">Loading ...</div>
+    )
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="KJj70dBgRPo" />
+            <Youtube videoId={data?.lesson.videoId} /> 
             <DefaultUi />
           </Player>
         </div>
@@ -18,22 +63,22 @@ export function Video() {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              Aula 1 - Abertura das Provas Chunin
+              {data?.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom
+              {data?.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://static.wikia.nocookie.net/naruto/images/6/61/Ibiki_Morino.png/revision/latest?cb=20130416184212&path-prefix=pt-br"
+                src={data?.lesson.teacher.avatarURL}
                 alt=""
               />
 
               <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">Ibiki Morino</strong>
-                <span className="text-gray-200 text-sm block">Líder do squadrão especial de tortura e interrogações</span>
+                <strong className="font-bold text-2xl block">{data?.lesson.teacher.name}</strong>
+                <span className="text-gray-200 text-sm block">{data?.lesson.teacher.bio}</span>
               </div>
             </div>
           </div>
